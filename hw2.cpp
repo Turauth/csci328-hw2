b951d6c043fb269070c75165bf29de688c631886
Turauth <93845548+Turauth@users.noreply.github.com>
2025-09-26 02:28:40 -0400

    I eventually paused work on other member functions of the linked list because it seems easier to test the functions by working on the insert function first. Total time spent on this project so far: 9.25 hrs.

--- hw2.cpp
+++ hw2.cpp
@@ -6,14 +6,9 @@
 
 int main()
 {
-    List list1 (14.67);
-    
-    list1.traverse(list1.head);
-    cout << list1.len(list1) << endl;
-    list1.del(0);
-    cout << list1.len(list1) << endl;
-    List list2 (7);
-    list2.del(1);
+    List erech (14.67);
+
+    erech.insert(2, 1);
 
     return 0;
 }
--- list.h
+++ list.h
@@ -6,80 +6,163 @@
 #include <iostream>
 using namespace std;
 
-// Define a struct for nodes and make the payload type float with a default value of -999999.
+// Define a struct for nodes and make the payload type float.
 struct Node
 {
-    float payload {-999999};
-    Node *link;
+    float payload {};
+    Node* link;
 };
 
 // Define class List to encapsulate linked list behavior.
-class List
-{
+class List {
+private:
+    Node temp {};
+    bool listIsEmpty { false };
+
 public:
-    Node head {};
+    Node* head;
 
     // Make a specified constructor that takes in a float number as a parameter and makes it the payload 
     // for the first element of the linked list.
     List(float payload) {
-        head.payload = payload;
-        head.link = nullptr;
+        head = new Node;
+        head->payload = payload;
+        head->link = nullptr;
     }
 
-    // Define a function to traverse through the whole list and print out its contents. This definition was
-    // developed using the recursive approach on this page:
-    // https://www.geeksforgeeks.org/dsa/traversal-of-singly-linked-list/.
-    void traverse(Node currentNode) {     
-        
-        // Print the current node data and return if the current node's link is null.
-        cout << currentNode.payload << endl;
-        if (currentNode.link == nullptr) {
-            return;
+    // Define a function to traverse through the whole list and print out its contents
+    // (https://www.geeksforgeeks.org/dsa/traversal-of-singly-linked-list/).
+    void traverse(Node* head) {       
+        while (head != nullptr) {
+            cout << head->payload;
+            if (head->link != nullptr) {
+                cout << " -> ";
+            }
+            head = head->link;
         }
-
-        // Print an arrow if there are more nodes in the list and move to the next node.
-        if (currentNode.link != nullptr) {
-            cout << " -> ";
-        }
-
-        traverse(*currentNode.link);
+        cout << endl;
     }
 
     // Define a function to return the length of the list.
-    int len(List list) {
+    int len() {
         int length { 0 };
-        Node currentNode { list.head };
+        Node* currentPointer { head };
 
-        if (currentNode.payload != -999999) {
-            length += 1;
-        }
-
-        while (currentNode.link != nullptr) {
-            Node currentNode = *currentNode.link;
+        while (currentPointer != nullptr) {
+            currentPointer = currentPointer->link;
             length +=1;
         }
 
         return length;
     }
 
-    // Define functions to delete and insert elements at a specified index in the list
+    // Define a function to delete elements at a specified index in the list
     // (https://www.geeksforgeeks.org/cpp/cpp-program-for-deleting-a-node-in-a-linked-list/).
-    void del(int index) {
+    /*void del(int index) {
+
         if (index == 0) {
-            if (&head == nullptr) {
-                cout << "List is empty; there is nothing to delete." << endl;
-                return;
+            if (listIsEmpty == true) {
+                cout << "The list is empty; there is nothing to delete." << endl;
             }
 
-            // Store current head in a temporary variable, move head to the next node, and delete the old head node.
-            Node temp = head;
-            head = *head.link;
-            delete &temp;
-            cout << "Head node deleted." << endl;
+            else {
+                // Store current head in a temporary variable, move head to the next node, and delete the old head node.
+                if (head.link != nullptr) {
+                    Node temp = head;
+                    head = *(head.link);
+                    temp.link = nullptr;
+                    temp.payload = 9.999;
+                    cout << "A new head has been elected." << endl;
+                }
+
+                else {
+                    head.payload = 9.999;
+                    listIsEmpty = true;
+                    cout << "The head payload was set to its default value and the list should be empty." << endl;
+                }
+            }
         }
         
-        else {
-            cout << "No nodes deleted." << endl;
+        else if (index > 0) {
+            Node currentNode { head };
+
+            for (int i { 0 }; i < index; i++) {
+
+                // If the index is found to be greater than the length of the list, return an appropriate message.
+                if (currentNode.link == nullptr) {
+                    cout << "The index is greater than the length of the list." << endl;
+                }
+
+                else {
+                    // Walk through the list until the index is reached or it is discovered that the index is greater than
+                    // the length of the list.                    
+                    Node nextNode { *(currentNode.link) };
+                    currentNode = nextNode;
+
+                    // When the next step would correspond to the index, set its payload value to the default and check whether 
+                    // the next node is the tail.
+                    if (i == index - 1) {
+                        nextNode.payload = 9.999;
+                        
+                        if (nextNode.link == nullptr) {
+                            // If the next node is the tail, cut the pointer to it.
+                            currentNode.link = nullptr;
+                            cout << "Tail node deleted" << endl;
+                        }
+
+                        else {
+                            // If the next node is not the head or the tail, move the link from the current node to link to
+                            // the node after the node being deleted and cut the link from the deleted node.
+                            currentNode.link = nextNode.link;
+                            nextNode.link = nullptr;
+                            cout << "Middle node deleted." << endl;
+                        }                            
+                    }
+                }
+            }
         }
-    }
+
+        else {
+            cout << "The index is out of range." << endl;
+        }
+    }*/
+
+    // Define a function to insert elements at a specified index in the list.
+    void insert(float payload, int index) {
+        Node* newNode = new Node;
+        newNode->payload = payload;
+        newNode->link = nullptr;
+
+        if (index == 0) {
+            // Make the pointer from the new node point to the old head node. Redefine the pointer to the head as pointing to the
+            // new node.
+            newNode->link = head;
+            head = newNode;
+            
+            cout << "A node was inserted at the head of the list." << endl;
+            return;
+        }
+
+        // For index > 0, find the node at position (index-1)
+        Node* currentPointer = head;
+
+        // Traverse to the node just before the insertion point.        
+        for (int i { 0 }; i < index - 1; i++) {
+            
+            // If the index is found to be greater than the length of the list, insert the node at the tail of the list.
+            if (currentPointer->link == nullptr) {
+                currentPointer->link = newNode;
+                
+                cout << "The index is greater than the length of the list. ";
+                cout << "The node was inserted at the tail of the list." << endl;
+                return;
+            }
+            currentPointer = currentPointer->link;
+        }
+            // When the next node would correspond to the index, reassign the link of the current node to the
+            // inserted node.
+            newNode->link = currentPointer->link;
+            currentPointer->link = newNode;
+            cout << "A node was inserted between the head and tail of the list." << endl;
+    } 
 };
